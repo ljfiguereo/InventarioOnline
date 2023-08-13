16e944fcf60d3f0591f990b02e4d967cb3788baa
Luis Junior Figuereo <ljfiguereo@gmail.com>
2023-08-13 14:24:01 -0400

    first Commit - Crear Proyecto

---
+++ InventarioOnline/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace InventarioOnline.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
